64d3b2e80edcf01ebc4b0f08c3900180b8259ed4
Mohammad Adiyudha <18102238@ittelkom-pwt.ac.id>
2021-08-23 10:13:15 +0700

    require beberapa file php

---
+++ init.php
@@ -0,0 +1,9 @@
+<?php 
+require_once ('../class/config.php');
+require_once ('../class/tampildata.php');
+require_once ('../class/tambahclass.php');
+require_once ('../class/aksi.php');
+require_once ('../class/akunclass.php');
+
+
+?>
